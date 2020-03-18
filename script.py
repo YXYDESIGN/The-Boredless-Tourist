2bf16a3767c6bc418af12984e368090b6113616b
codecademy <ccuser@codecademy.com>
2020-03-18 04:19:39 +0000

    function_1

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
-destinations = [“Paris, France”, “Shanghai, China”, “Los Angeles, USA”, “São Paulo, Brazil”, “Cairo, Egypt”]
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+  destination_index = 0
+  for place in destinations:
+    if destination == place:
+      destination_index = destinations.index(place)
+      return destination_index
+    
+print(get_destination_index('Hyderabad, India'))
+  
