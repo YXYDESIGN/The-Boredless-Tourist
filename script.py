26c1a5aea4fd7cd8acdecd386c2c5c7d42079cf6
codecademy <ccuser@codecademy.com>
2020-03-18 05:11:52 +0000

    Added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
 def get_destination_index(destination):
   destination_index = 0
   for place in destinations:
@@ -9,3 +10,10 @@ def get_destination_index(destination):
     
 print(get_destination_index('Hyderabad, India'))
   
+def get_traveler_location(traveler):
+  name,traveler_destination,like = traveler
+  return traveler_destination
+
+test_destination_index = get_destination_index(get_traveler_location(test_traveler))
+
+print(test_destination_index)
