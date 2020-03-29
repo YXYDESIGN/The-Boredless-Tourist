2f06de0e6be7ad10ba61a67f964335bbdffdb616
codecademy <ccuser@codecademy.com>
2020-03-29 14:17:04 +0000

    Complete version

--- script.py
+++ script.py
@@ -48,7 +48,6 @@ def find_attractions(destination,interests):
   destination_index = get_destination_index(destination)
   attractions_in_city = attractions[destination_index]
   attractions_with_interest = []
-
   for attraction in attractions_in_city:
     possible_attraction = attraction
     attraction_tags = attraction[1]
@@ -60,6 +59,17 @@ def find_attractions(destination,interests):
 la_arts = find_attractions("Shanghai, China",["garden","museum"])
 print(la_arts)
 
-    
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination,traveler_interests)
+  interests_string = 'Hi ' + traveler[0] + ", we think you'll like these places around " + traveler_destination + ": "
+  for traveler_attraction in traveler_attractions:
+    interests_string = interests_string + traveler_attraction
+  return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+
+print(smills_france)
     
   
