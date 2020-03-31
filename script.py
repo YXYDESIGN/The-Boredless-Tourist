2435d30be26703e0a97e147aff27aefd0ead4571
Xingyu Yang <yxy19944991@gmail.com>
2020-03-31 13:42:57 +0800

    delete a line

--- script.py
+++ script.py
@@ -69,7 +69,6 @@ def get_attractions_for_traveler(traveler):
   return interests_string
 
 smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
-
 print(smills_france)
     
   
